2ab3efb0bec83b45994e72e442f43a38101ea0e5
coljamkop <coljamkop@gmail.com>
2017-07-01 17:50:06 -0600

    added angular support

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 var pool = require('./db');
 
 var server = app.listen(process.env.PORT || 8080, function() {
