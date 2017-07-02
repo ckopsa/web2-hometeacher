828d4f3897cde875be4f5257252a9409a0beab22
coljamkop <coljamkop@gmail.com>
2017-07-01 19:42:38 -0600

    Updated home-teacher to see if that works better

--- src/app/home-teacher.ts
+++ src/app/home-teacher.ts
@@ -2,7 +2,7 @@ export class HomeTeacher {
     id: number;
     firstname: string;
     lastname: string;
-    phoneNumber: string;
-    postalAddress: string;
-    companionshipId: number;
+    phone_number: string;
+    postal_address: string;
+    companionship_id: number;
 }
