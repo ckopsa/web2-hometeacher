fb26126ce5a90b3360cef3698427123f3ce6701b
coljamkop <coljamkop@gmail.com>
2017-07-13 10:12:38 -0600

    Forgot to include providers

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from '../services/auth.service';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: ['./login.component.css'],
+    providers: [AuthService]
 })
 export class LoginComponent implements OnInit {
     email = '';
